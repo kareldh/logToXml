a39e674000219034d9a6d1127d7e2d8f1fdd877b
Karel D'haene <karel.dhaene@gmail.com>
2019-03-05 18:53:47 +0100

    initial spring starter project

---
+++ logToXml/src/main/java/com/example/logToXml/LogToXmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.logToXml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogToXmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogToXmlApplication.class, args);
+	}
+
+}
