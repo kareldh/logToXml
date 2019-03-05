0649d885dec1f7de1526a0685dd9fda8008cc4d2
Karel D'haene <karel.dhaene@gmail.com>
2019-03-06 00:01:25 +0100

    basic parsing of the logfile and creation of a report

--- logToXml/src/main/java/com/example/logToXml/LogToXmlApplication.java
+++ logToXml/src/main/java/com/example/logToXml/LogToXmlApplication.java
@@ -1,13 +1,28 @@
 package com.example.logToXml;
 
+import com.example.logToXml.model.Report;
+import com.example.logToXml.reader.LogFileReporter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LogToXmlApplication {
+public class LogToXmlApplication implements CommandLineRunner{
+
+	private final LogFileReporter logFileReporter;
+
+	@Autowired
+	public LogToXmlApplication(LogFileReporter logFileReporter) {
+		this.logFileReporter = logFileReporter;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogToXmlApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args){
+		Report r = logFileReporter.reportLogFile("D:\\Schooldocumenten\\Github_projects\\server\\server.log");
+	}
 }
